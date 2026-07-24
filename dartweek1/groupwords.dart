6d2ab94fc69c2047028a025f3eef67dd12708146
Mahbubi <mahbubim06@gmail.com>
2026-07-24 15:55:19 +0530

    Added new file

--- dartweek1/groupwords.dart
+++ dartweek1/groupwords.dart
@@ -1,4 +1,13 @@
 void main(){
 List<String>words=["cat", "apple", "dog", "banana"];
 Map<int, List<String>>lengthwords={};
+for(String word in words ){
+    int length=word.length;
+    if(lengthwords.containsKey(length)){
+        lengthwords[length]!.add(word);
+    }else{
+        lengthwords[length]=[word];
+    }
+}
+print(lengthwords);
 }
